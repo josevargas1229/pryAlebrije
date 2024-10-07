f7e289081fab8c3a3636e1f30e3ab2ddb046fcd0
BrandonArmandoLaraRosas <20221091@uthh.edu.mx>
2024-10-07 17:40:28 -0600

    modified

---
+++ src/app/recupera/recupera.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recupera',
+  standalone: true,
+  imports: [],
+  templateUrl: './recupera.component.html',
+  styleUrl: './recupera.component.css'
+})
+export class RecuperaComponent {
+
+}
