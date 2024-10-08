cbe0156f1a1c92431bef5c5a7396ff8c6823df02
BrandonArmandoLaraRosas <20221091@uthh.edu.mx>
2024-10-08 12:07:32 -0600

    perfil y dashboard

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,16 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+interface Service {
+  title: string;
+  description: string;
+  icon: string;
+}
 @Component({
   selector: 'app-perfil',
   standalone: true,
+  imports:[CommonModule],
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css'],
 })
+
+
 export class PerfilComponent {
-  userName = 'Brandon Lara';
-  userEmail = 'brandon.lara@example.com';
-  userFullName = 'Brandon Armando Lara Rosas';
-  userAddress = '1234 Calle Falsa, Ciudad, País';
-  userPhone = '+52 123 456 7890';
-  userBirthday = '01/01/1990';
+  services: Service[] = [
+    {
+      title: 'Politica de Privacidad',
+      description: 'Este apartado se encarga de editar,actualizar o eliminar las politicas de privacidad',
+      icon: 'fas fa-search'
+    },
+    {
+      title: 'Terminos y Condiciones',
+      description: 'Este apartado se encarga de editar,actualizar o eliminar los terminos y condiciones ',
+      icon: 'fas fa-pencil-alt'
+    },
+    {
+      title: 'Deslinde Legal',
+      description: 'Este apartado se encarga de editar,actualizar o eliminar el deslinde legal sobre la empresa',
+      icon: 'fas fa-share-alt'
+    },
+    {
+      title: 'Perfil de la Empresa',
+      description: 'Este apartado se encarga de editar,actualizar o eliminar el perfil de la empresa',
+      icon: 'fas fa-mouse-pointer'
+    }
+  ];
+
+  selectedService: Service | null = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showServiceDetails(service: Service): void {
+    this.selectedService = service;
+  }
 }
