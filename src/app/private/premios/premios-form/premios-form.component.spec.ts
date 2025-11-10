08283e339590599b05d60c21917dd76a417403c6
BrandonLara <20221091@uthh.edu.mx>
2025-11-09 18:35:49 -0600

    Mejora las pruebas del componente PremiosForm y Premios al agregar módulos y servicios necesarios.

--- src/app/private/premios/premios-form/premios-form.component.spec.ts
+++ src/app/private/premios/premios-form/premios-form.component.spec.ts
@@ -1,16 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PremiosFormComponent } from './premios-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { PremioService } from '../premio.service';
+import { Premio } from '../models';
 
 describe('PremiosFormComponent', () => {
   let component: PremiosFormComponent;
   let fixture: ComponentFixture<PremiosFormComponent>;
 
+  const premioServiceMock: Partial<PremioService> = {
+    getPremio: (_id: number) => of({} as Premio),
+    createPremio: (_body: Partial<Premio>) =>
+      of({ id: 1, ..._body } as Premio),
+
+    updatePremio: (_id: number, _body: Partial<Premio>) =>
+      of({ id: _id, ..._body } as Premio),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PremiosFormComponent]
-    })
-    .compileComponents();
+      declarations: [PremiosFormComponent],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: PremioService, useValue: premioServiceMock },
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: { get: () => null } } }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PremiosFormComponent);
     component = fixture.componentInstance;
--- src/app/private/premios/premios/premios.component.spec.ts
+++ src/app/private/premios/premios/premios.component.spec.ts
@@ -1,16 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PremiosComponent } from './premios.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { of } from 'rxjs';
+import { PremioService } from '../premio.service';
+import { RuletaPremiosAdminService } from '../ruleta-premios-admin.service';
+import { Premio, RuletaDTO, SegmentoRuletaDTO } from '../models';
 
 describe('PremiosComponent', () => {
   let component: PremiosComponent;
   let fixture: ComponentFixture<PremiosComponent>;
 
+  const premioServiceMock: Partial<PremioService> = {
+    listPremios: () => of([] as Premio[]),
+    deletePremio: (_id: number) => of({} as Object)
+  };
+
+  const ruletaServiceMock: Partial<RuletaPremiosAdminService> = {
+    listRuletas: () => of([] as RuletaDTO[]),
+    listSegmentos: (_id: number) => of([] as SegmentoRuletaDTO[]),
+    deleteSegmento: (_ruletaId: number, _segId: number) => of({} as Object)
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PremiosComponent]
-    })
-    .compileComponents();
+      declarations: [PremiosComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatDialogModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: PremioService, useValue: premioServiceMock },
+        { provide: RuletaPremiosAdminService, useValue: ruletaServiceMock }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PremiosComponent);
     component = fixture.componentInstance;
