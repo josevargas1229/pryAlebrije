87cae9c8e343a21256011f38a9a934a840d89dc4
BrandonLara <20221091@uthh.edu.mx>
2025-11-09 20:07:50 -0600

    spec.ts pruebas

--- src/app/private/reportes-gamificacion/reportes-gamificacion.component.spec.ts
+++ src/app/private/reportes-gamificacion/reportes-gamificacion.component.spec.ts
@@ -1,16 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReportesGamificacionComponent } from './reportes-gamificacion.component';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { NgChartsModule } from 'ng2-charts';
-import { MatFormField } from '@angular/material/select';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { ReportesGamificacionService } from './reportes-gamificacion.service';
 
 describe('ReportesGamificacionComponent', () => {
   let component: ReportesGamificacionComponent;
@@ -20,19 +17,13 @@ describe('ReportesGamificacionComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ReportesGamificacionComponent],
       imports: [
+        CommonModule,
+        FormsModule,
         HttpClientTestingModule,
         BrowserAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatCardModule,
-        MatIconModule,
-        MatSelectModule,
-        MatTableModule,
-        MatButtonModule,
-        MatProgressSpinnerModule,
-        NgChartsModule,
-        MatFormField
       ],
+      providers: [ReportesGamificacionService],
+      schemas: [NO_ERRORS_SCHEMA], 
     }).compileComponents();
 
     fixture = TestBed.createComponent(ReportesGamificacionComponent);
